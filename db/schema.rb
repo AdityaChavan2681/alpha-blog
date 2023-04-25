9d4e44b32bb7f4437de9715b2b2e880fc9c53556
AdityaChavan2681 <entp847@gmail.com>
2023-04-25 15:18:56 +0530

    new migration to add two more columns to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_25_092632) do
+ActiveRecord::Schema.define(version: 2023_04_25_094426) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
