9c1b0ec50a4ccbbc28e8362a58a4e00820a6f754
AdityaChavan2681 <entp847@gmail.com>
2023-06-22 23:15:41 +0530

    Small changes in user privileges

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,9 +52,9 @@ class ArticlesController < ApplicationController
   end
   
   def require_same_user
-    if current_user != @article.user
+    if current_user != @user
       flash[:alert] = "You can only edit or delete your own article"
-      redirect_to @article
+      redirect_to articles_path
     end
   end
 
